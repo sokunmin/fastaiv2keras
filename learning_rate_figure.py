8620018539d9b55d6e53d3edb62220073d75df24
Chun-Ming <sokunmin@gmail.com>
2019-04-12 12:19:50 +0800

    Add sinusoid with decay

--- learning_rate_figure.py
+++ learning_rate_figure.py
@@ -38,50 +38,83 @@ def exp_triangular():
 # exp_triangular()
 # triangular()
 
-def get_cycle_range(max_iter, n_cycles, c_mul, paddding=0):
+def get_cycle_range2(max_iters, n_cycles, c_mul):
     # 100 / 12 = 0.0833, 1-0.0833=0.91667
     # 12 = 6 x 2 (segments)
 
     begin = - (100 / (n_cycles * c_mul)) / 100
-    end = 1 + begin + paddding
-    # return np.linspace(begin, end, num=max_iter)
-    return np.linspace(0, 1.0, num=max_iter)
+    end = 1 + begin
+    return np.linspace(begin, end, num=max_iters)
+    # return np.arange(begin, end, 0.01)
 
 
-def cosine_smooth2(max_iter=200, n_cycles=2):
+def get_cycle_range(max_iters, n_cycles, c_mul):
+
+    begin = - (max_iters / (n_cycles * c_mul)) / 100
+    end = 1 + begin
+    return np.linspace(begin, end, num=max_iters)
+
+
+
+def cosine_smooth2(max_iters=200, n_cycles=3):
     # https://stackoverflow.com/questions/51926684/plotting-sum-of-two-sinusoids-in-python
     # > additional vars for exp_decay
-    step_size = max_iter / (2 * n_cycles)
+    step_size = max_iters / (2 * n_cycles)
     cycle = np.floor(1 + iterations / (2 * step_size))
 
-    x1 = get_cycle_range(max_iter, n_cycles, 4)
-    y1 = np.sin(np.pi * x1 * (n_cycles * 2))
+    x1 = get_cycle_range(max_iters, n_cycles, 4)
+    yt = np.sin(np.pi * x1 * (n_cycles * 2))
 
-    n_cycles *= 2
-    x2 = get_cycle_range(max_iter, n_cycles, 4)
-    y2 = np.sin(np.pi * x2 * (n_cycles * 2))
-
-    yt = y2 + y1
     yt = 1 - (yt / 2 + 0.5)
 
     # > [no exp decay]
     # lr = min_lr + (max_lr - min_lr) * np.maximum(0, (1 - yt)) * (gamma ** iterations)
 
     # > [half decay]
-    lr = min_lr + (max_lr - min_lr) * np.maximum(0, yt) # / 2 ** (cycle - 1)
+    lr = min_lr + (max_lr - min_lr) * np.maximum(0, yt) / 2 ** (cycle - 1)
     ax.plot(iterations, lr)
     plt.show()
 
 
-# cosine_smooth()
-# cosine_smooth2()
+def sinusoid(max_iters=200, n_cycles=2, c_mul=6):
+    # x = np.arange(0.0, 2.0, 0.01)
+    x = get_cycle_range(max_iters, n_cycles, c_mul)
 
-def sinusoid():
-    t = np.arange(0.0, 2.0, 0.01)
+    print('len(t): ', len(x))
+    y1 = np.sin(n_cycles * np.pi * x)
+    y2 = np.sin(n_cycles * 2 * np.pi * x)
+    y = (y1 + y2)
+    max_y, min_y = max(y), min(y)
+    y = y / (max_y + np.abs(min_y))
+    y = y + 0.5
 
-    y1 = np.sin(4 * np.pi * t)
-    y2 = np.sin(2 * np.pi * t)
-    ax.plot(t, y1 + y2)
+    lr = min_lr + (max_lr - min_lr) * np.maximum(0, y)# / 2 ** (cycle - 1)
+
+    ax.plot(iterations, lr)
     plt.show()
 
-sinusoid()
+
+def sinusoid_with_decay(max_iter=200, n_cycles=3):
+    # https://stackoverflow.com/questions/51926684/plotting-sum-of-two-sinusoids-in-python
+    step_size = max_iter / (2 * n_cycles)
+    cycle = np.floor(1 + iterations / (2 * step_size))
+
+    # 100 / 12 = 0.0833, 1-0.0833=0.91667
+    # 12 = 6 x 2 (segments)
+    xt = get_cycle_range2(max_iter, n_cycles, 4)
+    # xt = np.linspace(-0.0833, 0.91667, num=max_iter)
+    yt = 1 - (np.sin(np.pi * xt * (2 * n_cycles)) / 2 + 0.5)
+
+    # > [no exp decay]
+    # lr = min_lr + (max_lr - min_lr) * np.maximum(0, (1 - yt)) * (gamma ** iterations)
+
+    # > [half decay]
+    lr = min_lr + (max_lr - min_lr) * np.maximum(0, (1 - yt)) / 2 ** (cycle - 1)
+    ax.plot(iterations, lr)
+    plt.show()
+
+
+sinusoid_with_decay()
+# sinusoid()
+
+
